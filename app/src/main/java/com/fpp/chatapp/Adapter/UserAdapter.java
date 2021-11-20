85f3da10a5741afbface12e4d1f87651ec0034c5
Filippo0420 <fpawelec0@gmail.com>
2021-11-20 15:57:27 +0100

    part #6 done prepare to chat

--- app/src/main/java/com/fpp/chatapp/Adapter/UserAdapter.java
+++ app/src/main/java/com/fpp/chatapp/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.fpp.chatapp.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.fpp.chatapp.MessageActivity;
 import com.fpp.chatapp.Model.User;
 import com.fpp.chatapp.R;
 
@@ -44,6 +46,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         }else{
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
